ba644cbeded9d733e406b0570ba245d386b114e0
Jayant-Jeet <jayant.jeet.tomar.me@gmail.com>
2025-01-29 07:56:36 +0530

    QueueAtTheSchool

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+public final class Main {
+    public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		int t;
+		t= scr.nextInt();
+		while(t-->0) {
+			int n = scr.nextInt();
+        }
+        scr.close();
+    }
+}
